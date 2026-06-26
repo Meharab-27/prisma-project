efa193d76b132c0cc311f0496df9a5dfe9ca6fe4
Meharab-27 <meharabrahman100115@gmail.com>
2026-06-26 22:00:13 +0600

    implementing auth middleware for role based access control in api's

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -64,16 +64,16 @@ const registerUser = catchAsync(async(req:Request,res:Response,next:NextFunction
 
 const getMyProfile = catchAsync(async(req:Request,res:Response,next:NextFunction)=>{
        
-    const {accessToken} = req.cookies;
+    // const {accessToken} = req.cookies;
 
      
-       console.log(accessToken);
-       const verifiedToken = jwtUtils.verifyToken(accessToken,config.jwt_access_secret)
+    //    console.log(req.user,"user request");
+    //    const verifiedToken = jwtUtils.verifyToken(accessToken,config.jwt_access_secret)
 
-       if(typeof verifiedToken === "string"){
-        throw new Error(verifiedToken);
-       }
-      const profile = await userService.getMyProfileIntoDB(verifiedToken.id)
+    //    if(typeof verifiedToken === "string"){
+    //     throw new Error(verifiedToken);
+    //    }
+      const profile = await userService.getMyProfileIntoDB(req.user?.id as string)
 
     
       sendResponse(res, {
